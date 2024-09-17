d812453c5877794b64961cf1894def16e23d91ec
Maxiron <naarons800@gmail.com>
2024-09-17 20:09:50 +0100

    Refactor CRUD functions in admin API to handle exceptions and close database connections properly

--- admin_api/src/crud.py
+++ admin_api/src/crud.py
@@ -25,34 +25,58 @@ def get_borrowed_books_for_customer(customer_id):
 
 
 def create_book(db: Session, book: BookCreate):
-    db_book = Book(
-        id=str(uuid.uuid4()),
-        title=book.title,
-        author=book.author,
-        publisher=book.publisher,
-        category=book.category,
-        is_available=True
-    )
-    db.add(db_book)
-    db.commit()
-    db.refresh(db_book)
-    return db_book
+    try:
+        db_book = Book(
+            id=str(uuid.uuid4()),
+            title=book.title,
+            author=book.author,
+            publisher=book.publisher,
+            category=book.category,
+            is_available=True
+        )
+        db.add(db_book)
+        db.commit()
+        db.refresh(db_book)
+        return db_book
+    except Exception as e:
+        return {}
+    finally:
+        db.close()
 
 def delete_book(db: Session, book_id: str):
-    db_book = db.query(Book).filter(Book.id == book_id).first()
-    if not db_book:
+    try:
+        db_book = db.query(Book).filter(Book.id == book_id).first()
+        if not db_book:
+            return False
+        db.delete(db_book)
+        db.commit()
+        return True
+    except Exception as e:
         return False
-    db.delete(db_book)
-    db.commit()
-    return True
+    finally:
+        db.close()
 
 def mark_book_as_unavailable(db: Session, book_id: str):
-    db.query(Book).filter(Book.id == book_id).update({"is_available": False}, synchronize_session=False)
-    db.commit()
-    return True
+    try:
+        db.query(Book).filter(Book.id == book_id).update({"is_available": False}, synchronize_session=False)
+        db.commit()
+        return True
+    except Exception as e:
+        return False
+    finally:
+        db.close()
 
 def get_unavailable_books(db: Session):
-    query = db.query(Book).filter(Book.is_available == False).all()
+    try:
+        query = db.query(Book).filter(Book.is_available == False).all()
+    except Exception as e:
+        return SuccessResponse(
+            message= "An error occurred",
+            data={},
+            status_code=500
+        )
+    finally:
+        db.close()
     return SuccessResponse(
         message="Unavailable books retrieved successfully" if query else "No unavailable books found",
         data={
